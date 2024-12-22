6cbbc3786feb8a91516c77924a3fe74f808a7c8a
369px <168459560+369px@users.noreply.github.com>
2024-12-22 02:37:53 +0100

    improved macos SD formatting
    added better scripting. using disk identifier instead of name (to bypass issues when a name has spaces " " in it.

--- main.py
+++ main.py
@@ -26,6 +26,13 @@ def detect_sd_card():
             devices.extend([os.path.join(media_path, d) for d in os.listdir(media_path)])
     return devices
 
+def get_disk_identifier(volume_path):
+    result = subprocess.run(['diskutil', 'info', volume_path], capture_output=True, text=True)
+    for line in result.stdout.splitlines():
+        if line.startswith('   Device Identifier'):
+            return line.split(':')[1].strip()
+    return None
+
 def format_sd_card(sd_path):
     """Automatically format SD Card"""
     os_type = platform.system()
@@ -39,17 +46,24 @@ def format_sd_card(sd_path):
             """
             with open("format_script.txt", "w") as script_file:
                 script_file.write(script)
-
             subprocess.run("diskpart /s format_script.txt", check=True, shell=True)
             os.remove("format_script.txt")
 
-        elif os_type == "Darwin":
-            subprocess.run(["diskutil", "eraseDisk", "FAT32", "SDCARD", "MBRFormat", sd_path], check=True)
+        if os_type == "Darwin":
+            # Trova l'identificatore del disco per il volume
+            disk_identifier = get_disk_identifier(sd_path)
+            if disk_identifier:
+                applescript_command = f'sudo diskutil eraseDisk FAT32 {disk_identifier} MBRFormat'
+                subprocess.run(['osascript', '-e', applescript_command], check=True)
+                messagebox.showinfo("Formatting completed", "Your SD card has been formatted correctly!")
+            else:
+                messagebox.showerror("Error", "Unable to find the disk identifier.")
 
         elif os_type == "Linux":
             subprocess.run(["sudo", "mkfs.vfat", "-F", "32", sd_path], check=True)
 
         messagebox.showinfo("Formatting completed", "Your SD card has been formatted correctly!")
+
     except subprocess.CalledProcessError as e:
         messagebox.showerror("Error", f"Error while formatting: {e}")
 
