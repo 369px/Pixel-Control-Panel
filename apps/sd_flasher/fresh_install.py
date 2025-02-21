c28e16e339cc7c769f3ed6133a6555c0a5583890
369px <168459560+369px@users.noreply.github.com>
2025-02-21 03:25:02 +0100

    SD app improvements
    Added support for .7z files.
    Looks like nightlies and spruce 4 are working fine.

--- apps/sd_flasher/fresh_install.py
+++ apps/sd_flasher/fresh_install.py
@@ -1,9 +1,10 @@
 import requests, os, zipfile, threading, asyncio, subprocess, time
-import zipfile
+import zipfile, py7zr
 
 from apps.sd_flasher.update import get_latest_release_link
 from apps.sd_flasher.format import start_formatting
-from lib.sd_card import format_sd_card, get_volume_by_letter
+
+from lib.sd_card import format_sd_card, get_volume_by_letter, eject_sd
 
 def start_installing(sd_selector, display, dropped_file):
     # Start download on a different thread
@@ -17,13 +18,17 @@ def start_installing(sd_selector, display, dropped_file):
 
 def _install_update(sd_selector, display,dropped_file):
     if dropped_file != None:
-        print("Installing "+dropped_file)
-        unzip_file(sd_selector,dropped_file)
+        display.message("Installing "+ dropped_file)
+        #unzip_file(sd_selector,dropped_file,display)
+
+        download_thread = threading.Thread(target=unzip_file, args=(sd_selector, dropped_file, display))
+        download_thread.start()
+
     else:
         try:
             local_filename = os.path.join(sd_selector[0].get(), 'spruce.zip')
 
-            print("Start download...")
+            display.message("Starting download...")
 
             response = requests.get(get_latest_release_link(display), stream=True)
             response.raise_for_status()
@@ -47,23 +52,38 @@ def _install_update(sd_selector, display,dropped_file):
                             display.message(f"{downloaded_size / (1024 * 1024):.0f}/{total_size / (1024 * 1024):.0f} MB")
                             last_update = downloaded_size
 
-            display.message("Download completed, init unzip!")
-            unzip_file(sd_selector,local_filename)
+            display.message("Download completed, unzipping!")
+            #unzip_file(sd_selector,local_filename,display)
+            download_thread = threading.Thread(target=unzip_file, args=(sd_selector, dropped_file, display))
+            download_thread.start()
 
         except Exception as e:
-            print("Exception:", e)
+            display.message("Exception:", e)
 
-def unzip_file(sd_selector,local_filename):
-        # Estrazione del file ZIP
-        extract_path = os.path.join(sd_selector[0].get(), 'spruce')
 
-        if not os.path.exists(extract_path):
-            os.makedirs(extract_path)
+def unzip_file(sd_selector, local_filename,display):
+    display.message("Unzipping file...")
+    extract_path = os.path.join(sd_selector[0].get())
 
+    if not os.path.exists(extract_path):
+        os.makedirs(extract_path)
+
+    display.message("Checking if file is .zip or .7z...")
+    if local_filename.endswith('.zip'):
+        display.message("Unzippung .zip file...")
         with zipfile.ZipFile(local_filename, 'r') as z:
             z.extractall(path=extract_path)
+    elif local_filename.endswith('.7z'):
+        display.message("Unzippung .7z file... Be patient\nit can take some time...")
+        with py7zr.SevenZipFile(local_filename, mode='r') as z:
+            z.extractall(path=extract_path)
+    else:
+        raise ValueError("File format not supported")
 
-        try:
-            os.remove(local_filename)
-        except:
-            pass
+    try:
+        os.remove(local_filename)
+        # TODO: EJECT
+        #eject_sd(sd_selector[0].get(), sd_selector[0], sd_selector, display)
+        display.message("Done! Enjoy your spruce\nand check for firmware updates!")
+    except:
+        pass
