c87cd20d654031054d05e0b5240d4850cc99c03e
369px <168459560+369px@users.noreply.github.com>
2025-03-08 05:44:58 +0100

    fixed icon not showing on taskbar (windows)

--- lib/window_manager.py
+++ lib/window_manager.py
@@ -1,16 +1,24 @@
 import lib.gui.style as ui
 import platform
+from ctypes import windll
+
+GWL_EXSTYLE=-20
+WS_EX_APPWINDOW=0x00040000
+WS_EX_TOOLWINDOW=0x00000080
+
+def set_app_windows(root):
+    hwnd = windll.user32.GetParent(root.winfo_id())
+    style = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+    style = style & ~WS_EX_TOOLWINDOW
+    style = style | WS_EX_APPWINDOW
+    res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, style)
+    root.wm_withdraw()
+    root.after(10, lambda: root.wm_deiconify())
 
 def window_manager(root):
     ui.window()
     root.overrideredirect(True)
+	
 
     if platform.system() == "Windows":
-        def set_taskbar_icon():
-            import win32gui, win32con, win32api
-            hwnd = win32gui.GetForegroundWindow()
-            style = win32api.GetWindowLong(hwnd, win32con.GWL_EXSTYLE)
-            style |= win32con.WS_EX_APPWINDOW
-            win32api.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, style)
-
-        root.after(10, set_taskbar_icon)
+        root.after(10, lambda: set_app_windows(root))
